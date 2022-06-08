4c8c010917574b6cc1b6ae87550554cffde59794
indrajeet <indrajeetthorat83@gmail.com>
2022-06-08 16:08:27 +0530

    Initial commit

---
+++ src/MathematicalOperators.java
@@ -0,0 +1,20 @@
+public class MathematicalOperators {
+    public static void main(String[] args){
+        int x =52;
+        int y =85;
+        int result;
+
+        //addition
+        result = x+y;
+        System.out.println("Addition is"+result);
+        //substraction
+        result = x-y;
+        System.out.println("Sub is"+result);
+        //multiplication
+        result = x*y;
+        System.out.println("Multiply is"+result);
+        //division
+        result = x/y;
+        System.out.println("Division is"+result);
+    }
+}
