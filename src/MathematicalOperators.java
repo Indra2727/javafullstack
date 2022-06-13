b09cd2b17b120c8e661e6882856029143456a698
indrajeet <indrajeetthorat83@gmail.com>
2022-06-13 21:33:05 +0530

    DecisionMaking

--- src/MathematicalOperators.java
+++ src/MathematicalOperators.java
@@ -1,20 +1,25 @@
 public class MathematicalOperators {
-    public static void main(String[] args){
-        int x =52;
-        int y =85;
+    public static void main(String[] args) {
+
+        int x = 52;
+        int y = 85;
         int result;
 
         //addition
-        result = x+y;
-        System.out.println("Addition is"+result);
+        result = x + y;
+        System.out.println("Addition is" + result);
         //substraction
-        result = x-y;
-        System.out.println("Sub is"+result);
+        result = x - y;
+        System.out.println("Sub is" + result);
         //multiplication
-        result = x*y;
-        System.out.println("Multiply is"+result);
+        result = x * y;
+        System.out.println("Multiply is" + result);
         //division
-        result = x/y;
-        System.out.println("Division is"+result);
+        result = x / y;
+        System.out.println("Division is" + result);
+    }
+
+    public static void main1(String args[]) {
+        System.out.println("hellow");
     }
 }
