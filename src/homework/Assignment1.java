a51aa2cc1ff5a34209d7cc99cf37c5236afacb1d
indrajeet <indrajeetthorat83@gmail.com>
2022-06-15 00:21:05 +0530

    StarPattern1

--- src/homework/Assignment1.java
+++ src/homework/Assignment1.java
@@ -2,17 +2,17 @@ package homework;
 
 public class Assignment1 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
+        int a = 30;
+        int b = 45;
+        int c = 10;
 
         // Largest Number
         if (a > b && a > c)
             System.out.println(a + "  is Largest Number");
         else if (b > a && b > c)
-            System.out.println(b + "  is largest number");
+            System.out.println(b + "  is Largest Number");
         else
-            System.out.println(c + "  is largest number");
+            System.out.println(c + "  is Largest Number");
 
         //Smallest Number
         if (a < b && a < c)
---
+++ src/homework/StarPattern1.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class StarPattern1 {
+    public static void main(String[] args){
+        for(int i=3; i>0;i--){
+            for(int j=3; j>0;j--){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
