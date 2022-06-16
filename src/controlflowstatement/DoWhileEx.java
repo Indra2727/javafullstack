555cc0375d185e2c82890b6495a1ed73616b4168
indrajeet <indrajeetthorat83@gmail.com>
2022-06-16 16:17:45 +0530

    DoWhileEx

---
+++ src/controlflowstatement/DoWhileEx.java
@@ -0,0 +1,17 @@
+package controlflowstatement;
+
+public class DoWhileEx {
+    public static void main(String[] args){
+        int no =1;
+        do{
+            System.out.println("hiii");
+            no++;
+        }while (no<=5);//this condition is true then hiiii print 5 time
+
+        int no1 =15;
+        do{
+            System.out.println("hlooo");
+            no1++;
+        }while (no1<5);//this condition is true then hlooo  no print 5 time
+    }
+}
