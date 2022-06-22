b2640d824795d9b906df548e8a07094d521490b6
indrajeet <indrajeetthorat83@gmail.com>
2022-06-22 16:19:18 +0530

    DefaultConstructor

---
+++ src/constructor/Employee.java
@@ -0,0 +1,23 @@
+package constructor;
+//Default Constructor
+public class Employee {
+    int id;
+    String name;
+    String companyName;
+    Employee(){
+        System.out.println("In Default Constructor");
+    }
+    void display(){
+        System.out.println("Id :"+id);
+        System.out.println("Name :"+name);
+        System.out.println("CompanyName :"+companyName);
+    }
+}
+class EmployeeImpl{
+    public static void main(String[] args){
+        Employee e1=new Employee();
+        e1.display();
+        Employee e2=new Employee();
+        e2.display();
+    }
+}
