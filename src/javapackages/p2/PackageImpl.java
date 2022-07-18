34cce6bc71db02674943da4313f71730c0fc9d70
indrajeet <indrajeetthorat83@gmail.com>
2022-07-18 13:56:01 +0530

    Packages

---
+++ src/javapackages/p2/PackageImpl.java
@@ -0,0 +1,18 @@
+package javapackages.p2;
+
+//import javapackages.p1.A;
+//import javapackages.p1.B;
+import javapackages.p1.*;
+import javapackages.p1.p3.C;
+import javapackages.p1.p3.D;
+public class PackageImpl {
+    public static void main(String[] args) {
+        //B b = new B();
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        javapackages.p1.p3.A a1  = new javapackages.p1.p3.A();
+        System.out.println();
+    }
+}
