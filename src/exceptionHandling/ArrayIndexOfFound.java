baab73100cf2d867fc711212d4b51c700d3e66c3
indrajeet <indrajeetthorat83@gmail.com>
2022-07-25 18:48:25 +0530

    Exception Handling

---
+++ src/exceptionHandling/ArrayIndexOfFound.java
@@ -0,0 +1,8 @@
+package exceptionHandling;
+//Array Index of found Error
+public class ArrayIndexOfFound {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        System.out.println(a[6]);
+    }
+}
---
+++ src/exceptionHandling/NullPointerException.java
@@ -0,0 +1,8 @@
+package exceptionHandling;
+
+public class NullPointerException {
+    public static void main(String[] args) {
+        String name=null;
+        System.out.println(name.toLowerCase());
+    }
+}
