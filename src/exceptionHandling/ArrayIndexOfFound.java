e7dedafa339b414b728513f8c66324113582caaa
indrajeet <indrajeetthorat83@gmail.com>
2022-07-26 17:54:33 +0530

    Exception Handling

--- src/exceptionHandling/ArrayIndexOfFound.java
+++ src/exceptionHandling/ArrayIndexOfFound.java
@@ -3,6 +3,6 @@ package exceptionHandling;
 public class ArrayIndexOfFound {
     public static void main(String[] args) {
         int a[]={1,2,3,4,5};
-        System.out.println(a[6]);
+        System.out.println(a[5]);
     }
 }
