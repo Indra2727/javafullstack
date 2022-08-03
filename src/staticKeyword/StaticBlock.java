a78e1e4a0ca786d3a9251d08d8913199f0acadc7
indrajeet <indrajeetthorat83@gmail.com>
2022-08-03 17:25:58 +0530

    Static Block

--- src/staticKeyword/StaticBlock.java
+++ src/staticKeyword/StaticBlock.java
@@ -4,7 +4,6 @@ public class StaticBlock {
     static {
         System.out.println("In Static Block");
     }
-
     public static void main(String[] args) {
         System.out.println("In Main Method");
     }
