a21ee2a228efa8de0a4e2380a9710bd3b3ea5a3b
indrajeet <indrajeetthorat83@gmail.com>
2022-08-23 14:03:17 +0530

    Collections

---
+++ src/collection/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Pune");
+        list.add("mumbai");
+        list.add("Chennai");
+        list.add("kolkata");
+        list.add("Madras");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.add(3,"Bangalore");
+        System.out.println(list);
+
+        if (!list.isEmpty()){
+
+        }
+        System.out.println(list.get(2));
+        System.out.println(list.contains("Madras"));
+        System.out.println(list.indexOf("Bangalore"));
+        //list.containsAll()
+        list.remove("kolkata");
+        System.out.println(list);
+    }
+}
